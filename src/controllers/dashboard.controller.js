27218f6bef5cb4dcf4c5c32e09c726f007f31269
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-16 22:54:46 +0530

    feat: :sparkles: add get all videos uploaded by channel

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,81 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
+import { Types } from "mongoose";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+  const { _id } = req.user;
+  const { page, limit } = req.query;
 
-export {
-    getChannelStats, 
-    getChannelVideos
-    }
+  const pageNumber = Number(page) ? Number.parseInt(page) : 1;
+  const limitSize = Number(limit) ? Number.parseInt(limit) : 10;
+
+  const videos = await Video.aggregate([
+    {
+      $match: {
+        owner: new Types.ObjectId(_id.toString()),
+      },
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "video",
+        as: "comments",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        numberOfLikes: {
+          $size: "$likes",
+        },
+        numberOfComments: {
+          $size: "$comments",
+        },
+      },
+    },
+    {
+      $unset: ["likes", "comments"],
+    },
+    {
+      $skip: (pageNumber - 1) * limitSize,
+    },
+    {
+      $limit: limitSize,
+    },
+  ]);
+
+  return res.status(200).json(
+    new ApiResponse(200, {
+      videos,
+    })
+  );
+});
+
+export { getChannelStats, getChannelVideos };
